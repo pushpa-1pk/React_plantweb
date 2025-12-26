2390899a19386a2a17ae167448279043adcfd308
pushpa-1pk <pushpakaithal0@gmail.com>
2025-12-26 18:13:02 +0530

    Project change new

--- src/App.jsx
+++ src/App.jsx
@@ -16,31 +16,34 @@ function App() {
     <>
       {/* Background image wrapper */}
       <div className="min-w-screen min-h-screen bg-cover bg-top bg-no-repeat bg-[url('/src/assets/images/background.jpg')]">
-        <Navbar />
-        <Hero />
-        <Review />
-        <SectionTitle title="Our Trendy plants" />
+        <div className="max-w-[1200px] mx-auto">
+          <Navbar />
+          <Hero />
+          <Review />
+          <SectionTitle title="Our Trendy plants" />
 
-        <div className="flex flex-col gap-40 p-10 mt-14">
-          <TrendyPlants
-            title="For Your Desks Decorations"
-            description="I recently added a beautiful desk decoration plant to my workspace, and it has made such a positive difference!"
-            price="599"
-            image={PlantainLilies}
-            imagePosition="left"
-          />
+          <div className="flex flex-col gap-40 p-10 mt-14">
+            <TrendyPlants
+              title="For Your Desks Decorations"
+              description="I recently added a beautiful desk decoration plant to my workspace, and it has made such a positive difference!"
+              price="599"
+              image={PlantainLilies}
+              imagePosition="left"
+            />
 
-          <TrendyPlants
-            title="For Your Desks Decorations"
-            description="The greenery adds a touch of nature and serenity to my desk, making it feel more inviting and calming"
-            price="399"
-            image={Alovera}
-            imagePosition="right"
-          />
+            <TrendyPlants
+              title="For Your Desks Decorations"
+              description="The greenery adds a touch of nature and serenity to my desk, making it feel more inviting and calming"
+              price="399"
+              image={Alovera}
+              imagePosition="right"
+            />
+          </div>
         </div>
       </div>
 
       <div className="w-screen bg-[#1B2316] -mt-20">
+        <div className="max-w-[1200px] mx-auto">
         <SectionTitle title="Our Top Selling Plants"/>
         <TopSelling />
         <SectionTitle title="Customer Review"/>
@@ -48,7 +51,7 @@ function App() {
         <SectionTitle title="Our Best o2"/>
         <Best02 />
         <Footer />
-
+        </div>
       </div>
     </>
   );
